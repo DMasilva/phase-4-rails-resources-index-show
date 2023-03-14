840283e0c8ca5a975665e6a2067c95a830ee4f8b
David <david.owuor@student.moringaschool.com>
2023-03-14 14:53:38 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,14 +1,26 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
   def index
-    render json: Bird.all
+    render json: Bird.all, only: [:id, :name, :species]
   end
 
   def show
     birds = Bird.all.find(params[:id])
     if birds
-      render json: birds
+      render json: birds, only: [:id, :name, :species]
     else
       render json: {error: 'No valid input'} , status: :not_found
     end
+  end
+
+  def create
+    bird= Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 end
